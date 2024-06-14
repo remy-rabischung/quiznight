a68e85b16ab866ad96e9b7446dc1717394660fae
Rémy R <remy.rabischung@laplateforme.io>
2024-06-14 11:09:24 +0200

    Reglages liens footer et header

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top bg-dark text-white">
     <p class="col-md-4 mb-0 text-white">© 2024 QuizShot, Inc</p>
 
-    <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+    <a href="../public/index.php" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
         <img src="../assets/images/Quiz-logo.png" class="bi me-2" width="220" height="80">
     </a>
 
--- includes/header.php
+++ includes/header.php
@@ -12,7 +12,7 @@
   <header class="p-3 bg-dark text-white">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+        <a href="../public/index.php" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
           <img src="../assets/images/Quiz-logo.png" class="bi me-2" width="220" height="80" role="img" aria-label="Bootstrap" href="index.php"></img>
         </a>
 
